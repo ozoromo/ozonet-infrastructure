65253c388a7136b9e28754ce6c6b2d6f882ef910
ozoromo <gpg@ozoromo.com>
2026-05-10 18:07:43 +0200

    Added protected status to prod-secure-1

--- main.tf
+++ main.tf
@@ -10,8 +10,9 @@ resource "scaleway_instance_ip" "prod_secure_1" {
 }
 
 resource "scaleway_instance_server" "prod_secure_1" {
-  name   = "prod-secure-1"
-  type   = "PLAY2-PICO"
-  image  = "debian_bookworm"
-  ip_ids = [scaleway_instance_ip.prod_secure_1.id]
+  name      = "prod-secure-1"
+  type      = "PLAY2-PICO"
+  image     = "debian_bookworm"
+  ip_ids    = [scaleway_instance_ip.prod_secure_1.id]
+  protected = true
 }
